5f2bd43248cb6ea35ba1fae3b67c64732e2a6823
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-10-03 04:41:47 -0700

    0x06. Regular expression Regex DevOps
    0. Simply matching School
    1. Repetition Token #0
    2. Repetition Token #1
    3. Repetition Token #2
    4. Repetition Token #3
    5. Not quite HBTN yet
    6. Call me maybe
    7. OMG WHY ARE YOU SHOUTING?

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# 3. Repetition Token #2
+
+puts ARGV[0].scan(/h[A-Z]*tn/i).join
---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# 4. Repetition Token #3
+
+puts ARGV[0].scan(/h[A-Z]*[^O]n/i).join
