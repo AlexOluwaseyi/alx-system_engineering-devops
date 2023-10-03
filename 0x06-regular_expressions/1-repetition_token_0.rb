6e52f3a90bb3ad57a762292257e640c165d9fd59
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-10-03 04:47:48 -0700

    Hotfix: removed case insensitivity from regexes

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # 1. Repetition Token #0
 
-puts ARGV[0].scan(/hb[A-Z]+n/i).join
+puts ARGV[0].scan(/hb[A-Z]+n/).join
--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # 3. Repetition Token #2
 
-puts ARGV[0].scan(/h[A-Z]*tn/i).join
+puts ARGV[0].scan(/h[A-Z]*tn/).join
