a87bc0ccec4764f786cbb81b0b04fe65cffb137f
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-10-03 04:57:04 -0700

    Updated task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # 1. Repetition Token #0
 
-puts ARGV[0].scan(/hb[A-Z]+n/).join
+puts ARGV[0].scan(/hb[A-Z]{2,5}n/i).join
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # 2. Repetition Token #1
 
-puts ARGV[0].scan(/h[a-z]?tn/).join
+puts ARGV[0].scan(/h[a-z]?tn/i).join
