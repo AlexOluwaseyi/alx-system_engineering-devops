7d72cc81f47e4abccb3af9fffdcce851a2b2e67e
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-12-05 14:18:18 -0800

    New

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,26 @@
 # A puppet script to auto-configure new server with new header
 include stdlib
 
-package { "nginx":
-    ensure => 'installed',
+exec { 'update_server':
+    command  => 'apt-get -y update',
+    user     => 'root',
+    provider => 'shell'
 }
 
-file_line { "custom_header":
-    path  => '/etc/nginx/sites-enabled/defaul',
-    line  => 'add_header X-Served-By $hostname;',
-    after => 'listen [::]:80 default_server;',
+-> package { 'nginx':
+    ensure   => present,
+    provider => 'apt'
 }
 
-exec { "restart_nginx":
+-> file_line { 'custom_header':
+    ensure =>
+    path   => '/etc/nginx/sites-enabled/defaul',
+    after  => 'listen [::]:80 default_server;',
+    line   => 'add_header X-Served-By $hostname;'
+}
+
+-> exec { 'restart_nginx':
     path    => '/usr/bin',
     command => 'sudo service nginx restart',
-    require => [Package["nginx"], File_line["custom_header"]]
+    require => [Package'nginx'], File_line['custom_header']]
 }
