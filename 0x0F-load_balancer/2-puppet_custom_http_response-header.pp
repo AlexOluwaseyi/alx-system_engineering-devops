73903fe5e33864171bb210e1f6ac9cc7c50a8578
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-12-10 01:52:20 -0800

    Retry v0.4

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,19 +1,21 @@
 # Installs a Nginx server with custome HTTP header
 
-class { 'nginx':
-    ensure => installed,
+#puppet advance
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
 }
-
-$file_path = '/etc/nginx/sites-available/default'
-$target_line = 'listen [::]:80 default_server;'
-$line_to_insert = 'add_header X-Served-By $hostname;'
-
-file { $file_path:
-    ensure => present
+-> package {'nginx':
+  ensure => present,
 }
-
-exec { 'insert_line_after_target':
-    command => "sed -i '/${target_line}/a ${line_to_insert}' ${file_path}",
-    unless  => "grep -q '${line_to_insert}' ${file_path}",
-    require => File[$file_path],
+-> file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
+}
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
